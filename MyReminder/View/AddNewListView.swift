08a0821e38ca2555503e20236a41ff12a159c7b3
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-07-10 01:03:48 +0600

    design add new list

--- MyReminder/View/AddNewListView.swift
+++ MyReminder/View/AddNewListView.swift
@@ -9,11 +9,23 @@ import SwiftUI
 
 struct AddNewListView: View {
     @State private var name: String = ""
+    @State private var selectedColor: Color = .gray
     
     var body: some View {
         VStack {
             Image(systemName: "line.3.horizontal.circle.fill")
+                .foregroundStyle(selectedColor)
+                .font(.system(size: 100))
+            
+            TextField("List Name", text: $name)
+                .multilineTextAlignment(.center)
+                .textFieldStyle(.roundedBorder)
+            
         }
+        .padding(30)
+        .clipShape(RoundedRectangle(cornerRadius: 10.0, style: .continuous))
+        
+        Spacer()
     }
 }
 
