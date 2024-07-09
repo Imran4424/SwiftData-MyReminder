10b17c3f879c2cc639020e4ee250eacd281eff0b
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-07-10 00:00:42 +0600

    project init

---
+++ MyReminder/MyReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyReminderApp.swift
+//  MyReminder
+//
+//  Created by Shah Md Imran Hossain on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MyReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
