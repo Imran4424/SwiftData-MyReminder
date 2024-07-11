72907cbe33d4439d491fbafe4516fee014ef5da1
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-07-11 18:28:16 +0600

    preparing list view to navigate

--- MyReminder/View/MyListView.swift
+++ MyReminder/View/MyListView.swift
@@ -17,13 +17,19 @@ struct MyListView: View {
                     Spacer()
                     Text("No reminders found")
                 } else {
-                    ForEach(myLists) { myListItem in
-                        MyListCellView(myList: myListItem)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .padding(.leading, 10)
-                            .font(.title3)
-                        
-                        Divider()
+                    ForEach(myLists) { myList in
+                        NavigationLink(value: myList) {
+                            MyListCellView(myList: myList)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding(.leading, 10)
+                                .font(.title3)
+                            
+                            Divider()
+                        }
+                    }
+                    .scrollContentBackground(.hidden)
+                    .navigationDestination(for: MyList.self) { myList in
+                        Text(myList.name)
                     }
                 }
             }
