61996a7203d5d6b4e4a234a2775afd6c0bdee87a
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-07-11 12:58:39 +0600

    design add new list view

--- MyReminder/View/AddNewListView.swift
+++ MyReminder/View/AddNewListView.swift
@@ -8,27 +8,64 @@
 import SwiftUI
 
 struct AddNewListView: View {
+    @Environment(\.dismiss) var dismiss
+    
     @State private var name: String = ""
-    @State private var selectedColor: Color = .gray
+    @State private var selectedColor: Color = .red
+    
+    let onSave: (String, Color) -> Void
+    
+    private var isFormValid: Bool {
+        return name.isEmpty ? false : true
+    }
     
     var body: some View {
         VStack {
-            Image(systemName: "line.3.horizontal.circle.fill")
-                .foregroundStyle(selectedColor)
-                .font(.system(size: 100))
+            VStack {
+                Image(systemName: "line.3.horizontal.circle.fill")
+                    .foregroundStyle(selectedColor)
+                    .font(.system(size: 100))
+                
+                TextField("List Name", text: $name)
+                    .multilineTextAlignment(.center)
+                    .textFieldStyle(.roundedBorder)
+                
+            }
+            .padding(30)
+            .clipShape(RoundedRectangle(cornerRadius: 10.0, style: .continuous))
             
-            TextField("List Name", text: $name)
-                .multilineTextAlignment(.center)
-                .textFieldStyle(.roundedBorder)
+            ColorPickerView(selectedColor: $selectedColor)
             
+            Spacer()
         }
-        .padding(30)
-        .clipShape(RoundedRectangle(cornerRadius: 10.0, style: .continuous))
-        
-        Spacer()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .toolbar {
+            
+            ToolbarItem(placement: .topBarLeading) {
+                Button("Close") {
+                    dismiss()
+                }
+            }
+            
+            ToolbarItem(placement: .principal) {
+                Text("New List")
+                    .font(.headline)
+            }
+            
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("Save") {
+                    // save the list to SwiftData
+                    onSave(name, selectedColor)
+                    dismiss()
+                }
+                .disabled(!isFormValid)
+            }
+        }
     }
 }
 
 #Preview {
-    AddNewListView()
+    NavigationStack {
+        AddNewListView(onSave: { (_, _) in })
+    }
 }
